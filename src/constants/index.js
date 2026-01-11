6d97b791aecaf99feb546a96f8a2a7df29d10655
mirjalolinomjonov <mirjalolinomjonov@gmail.com>
2026-01-11 13:27:50 +0500

    Add constants for table column definitions

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+export const COLUMNS = [
+  { name: 'id', label: 'ID', field: 'id', align: 'left' },
+  { name: 'user_id', label: 'User ID', field: 'userId', align: 'center' },
+  { name: 'title', label: 'Title', field: 'title', align: 'left' },
+  { name: 'actions', label: 'Actions', field: 'actions' },
+]
