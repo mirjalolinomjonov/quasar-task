eb89bf3b44d33106c3ef461b767d317cd9e6a808
mirjalolinomjonov <mirjalolinomjonov@gmail.com>
2026-01-11 13:27:00 +0500

    Add default layout with sidebar and header components

---
+++ src/layouts/default.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue'
+import SidebarMenu from 'src/components/SidebarMenu.vue'
+
+// ===* DATA *===
+const leftDrawerOpen = ref(false)
+
+// ===* METHODS *===
+const toggleLeftDrawer = () => {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
+</script>
+
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title> Quasar App </q-toolbar-title>
+
+        <q-avatar style="cursor: pointer">
+          <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+        </q-avatar>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <SidebarMenu />
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
